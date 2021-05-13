89d2d0fc9d35117ff6071f14f94a09293577abbe
PaulRohack <61423644+paulrohack@users.noreply.github.com>
2021-05-13 23:07:29 +0530

    Add files via upload

---
+++ Mp3_dl.py
@@ -0,0 +1,19 @@
+import os
+
+run = True
+
+path = 'G:\Music\DownloadedMusic'
+try:
+    os.mkdir(path)
+except FileExistsError:
+    print("Path already exists")
+
+while run:
+    ask = input("[Y]outube or [Q]uit :")
+    if ask.upper() == 'Y':
+        url = input("YOUTUBE URL :  ")
+        f_url = ("youtube-dl -o G:\Music\DownloadedMusic\%(title)s.%(ext)s -x --audio-format mp3 " + url)
+        os.system(f_url)
+    else:
+        run = False
+
