2322b82709761ece6846bf913d19531d4988700b
PaulRohack <paulrock@gmail.com>
2021-06-11 09:22:57 +0530

    updated

--- LogisticMap.py
+++ LogisticMap.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from matplotlib import pyplot as plt
 import numpy as np
 x = 0.2
@@ -17,24 +16,4 @@ for ln in range(len(r)):
 plt.figure("Logistic Map")
 plt.title("x = xr(1 - xn)")
 plt.plot(xn, '.', markersize=0.05)
-=======
-from matplotlib import pyplot as plt
-import numpy as np
-x = 0.2
-i = 500
-r = np.linspace(1, 4, 1000)
-c = 0.1
-cn = []
-xn = []
-for ln in range(len(r)):
-    for n in range(i):
-        x = r[ln]*x*(1-x)
-        c += 0.1
-        # print(x)
-        cn.append(c)
-        xn.append(x)
-plt.figure("Logistic Map")
-plt.title("x = xr(1 - xn)")
-plt.plot(xn, '.', markersize=0.05)
->>>>>>> 1716e3ac153e6b6805d02dd7397be643a7ac4293
 plt.show()
