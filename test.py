079cc73e1333ab0ff48f768b003f4f0583dc0d62
PaulRohack <paulrock@gmail.com>
2021-06-11 11:19:34 +0530

    updated

--- test.py
+++
@@ -1,7 +0,0 @@
-import pyautogui, cv2
-import numpy as np
-image = pyautogui.screenshot()
-image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-cv2.imshow('s', image)
-cv2.waitKey(0)
-#  cv2.imwrite("in_memory_to_disk.png", image)
